92c60f087ff783089390bace25e2a83d62102586
Durgesh Kumar <68027282+durgesh0187@users.noreply.github.com>
2020-08-27 23:55:36 +0530

    Durgesh Kumar

---
+++ Calendar.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import calendar
+
+text = calendar.calendar(2020)
+root=Tk()
+root.geometry("500x600")
+root.title("CALENDAR")
+labell=Label(root,text="CALENDAR",bg='dark gray',font=("times",28,'bold'))
+labell.grid(row=1,column=1)
+root.config(background='white')
+l1=Label(root,text=text,font="Consolas 10")
+l1.grid(row=2,column=1,padx=20)
+root.mainloop()
+
+
+
